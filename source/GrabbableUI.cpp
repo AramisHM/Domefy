22c805ff8a47b277c4589b0307f14549e66ea18b
Aramis <aramishm@gmail.com>
2019-05-04 22:38:11 -0300

    Class for grabbable ui

---
+++ source/GrabbableUI.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: GrabbableUI.cpp
+ * Project: FpMED - Framework for Distributed Multiprojection Systems
+ * File Created: Saturday, 4th May 2019 12:58:36 am
+ * Author: Aramis Hornung Moraes (aramishm@gmail.com)
+ * -----
+ * Last Modified: Saturday, 4th May 2019 1:04:50 am
+ * Modified By: Aramis Hornung Moraes (aramishm@gmail.com>)
+ * -----
+ * Copyright 2014 - 2019 Aramis Hornung Moraes
+ */
+
+// This file contains the implementation for the grabbable UI interface for the
+// fulldome platform
