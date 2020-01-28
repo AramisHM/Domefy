45fc39b56d17e2e774f2e742b11092767837a21d
Aramis <aramishm@gmail.com>
2020-01-28 19:44:36 +0000

    WIP

--- webserver/frontwebapp/static/config.js
+++ webserver/frontwebapp/static/config.js
@@ -1,6 +1,6 @@
 // server host and port that contains the backend
-serviceIPPort = "192.168.15.5:9090";
-frontendIPPort = "192.168.15.5:3000";
+serviceIPPort = "10.150.160.89:9090";
+frontendIPPort = "10.150.160.89:3000";
 commandsEndpoint = "/setExampleTextMessage";
 cmdurlStartDomefy = "/StartScriptApplication";
 cmdurlStopDomefy = "/KillAllApplicationProcesses";
